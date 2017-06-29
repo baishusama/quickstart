1a90f08963223c9a2e2dfa0add87fb3c8cd1a9cf
baishusama <baishusama@outlook.com>
2017-06-29 10:49:39 +0800

    completed hero-proj's http part 7.2 including update, create, delete method.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -30,4 +30,9 @@ export class HeroDetailComponent implements OnInit {
     goBack(): void {
         this.location.back(); // [dxy][???] CanDeactivate
     }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
+    }
 }
